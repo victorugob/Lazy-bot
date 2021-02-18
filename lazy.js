67b2b274b841c7147dfec4e112a7aefa0816ad9b
Victor Hugo <57298432+victorugob@users.noreply.github.com>
2021-02-17 23:11:41 -0300

    Update lazy.js

--- lazy.js
+++ lazy.js
@@ -1,4 +1,11 @@
 const puppeteer = require('puppeteer');
+// Change const's value accordingly
+const username = "username";
+const password = "password";
+const devEmail = "email@email.com";
+const emailPw = "emailpw";
+const myInstance = "dev103039"
+const instance = "https://" + myInstance + ".service-now.com/login";
 
 function delay(time) {
   return new Promise(function(resolve) {
@@ -7,45 +14,51 @@ function delay(time) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch( {headless: false} ); 
+  const browser = await puppeteer.launch(); 
   
 
   /* Use o código abaixo dentro de  .launch() para que abra a aba para ver o processo
      Sem esse código o bot rodará as instruções no background
   {headless: false, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'} 
+  ou
+  { headless: false, defaultViewport: null, args: [
+    '--window-size=1360,768',
+  ] } 
    */
 
 
   const page = await browser.newPage();
-  await page.goto('https://yourinstance.service-now.com/login'); // atentar ao <numero da sua instancia>  e substituir a que irá usar
+  await page.goto(instance); // atentar ao <numero da sua instancia>  e substituir a que irá usar
   
   // Validate if instance is sleeping, if so, login to wake
   if(await (await page.$$('.instance-hibernating-page')).length > 0) {
     console.log('Instance is sleeping, Bot will redirect and wake your stance')
     await page.goto('https://developer.service-now.com/login')
-    await page.type('#username', email)
+    await page.type('#username', devEmail)
     await page.click('#usernameSubmitButton') 
     await delay(2000)
-    await page.type('#password', password);
+    await page.type('#password', emailPw);
 
     await Promise.all([
       page.waitForNavigation(), 
       page.click('#submitButton')
     ]);
+
     console.log('Bot awakaned your instance, please wait a few minutes');
-  } 
-  console.log('Your stance is not sleeping')
-  await page.type('#user_name', username ) //Substitua username por seu user ou email de login
-  await page.type('#user_password', password ) //Substitua password por sua senha de login
+  } console.log('Your stance is not sleeping')
+
+  await page.type('#user_name', username ) 
+  await page.type('#user_password', password) 
 
   await Promise.all([
-    page.waitForNavigation(), // promise resolve quando navigation finaliza
-    page.click('#sysverb_login') // clicar no link causa uma navigation indireta
+    page.waitForNavigation(), 
+    page.click('#sysverb_login') 
   ]);
 
-  //await page.screenshot({path: 'confirmation/anything.png'}); //path e nome da screenshot
+  //await page.screenshot({path: 'confirmation/anything.png'}); 
 
-  console.log('Bot has finalized'); // adicione qualquer mensagem para saber quando a ação for finalizada
+  console.log('Bot has finalized'); //
 
   await browser.close();
+  
 })();
